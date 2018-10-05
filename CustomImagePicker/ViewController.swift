4d4fb6b1d894c03fc0ce0adc0e36df0fad6f4874
Rishabh Raj <rrishabh1800@gmail.com>
2018-10-05 23:16:57 +0530

    Initial Setup

--- CustomImagePicker/ViewController.swift
+++ CustomImagePicker/ViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let libraryButton : UIButton = {
+       let button = UIButton()
+       
+        button.setTitle("Library", for: .normal)
+        button.backgroundColor = .black
+        button.addTarget(self, action: #selector(handleLibraryPicker), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    let picker = UIImagePickerController()
+    
+    @objc func handleLibraryPicker() {
+        print(123)
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(picker, animated: true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .red 
+        
+        picker.delegate = self
+        view.addSubview(libraryButton)
+        libraryButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true 
+        libraryButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        libraryButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        libraryButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // To Be Completed
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 
 
